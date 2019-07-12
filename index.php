0756b10ecbb6a7b0d7d2a1dd63e334d752f13118
Abinesh <34151015+Abinesh-g@users.noreply.github.com>
2019-07-12 16:25:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
-<html>
-
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+	
+	$name=$_POST['name'];
+$psw=$_POST['psw'];
+if($name=="AAMEC" AND $psw=="AAMEC")
+{
+header("location:main.php");
+}
+else
+{
+echo "<script>alert('Invalid Username or Password')</script>"; 
+}
+}
+?>
+<!doctype html>
+<html lang="en">
   <head>
-
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -13,13 +28,6 @@
     <link href="css/bootstrap.min.css" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-      <meta name="description" content="W3hubs.com">
-  <meta name="keywords" content="HTML,CSS,boostrap3,boostrap4,Jquery Auto Manage Responsive Lightbox">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" type="text/css" href="css/gallery.css">
-  <link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
-  <script type="text/javascript" src="js/lightbox-plus-jquery.min.js"></script>
-    
 <title>Student Information </title>
 
 </head>
@@ -27,21 +35,22 @@
 <body class="" background="bg1.png">
 <nav class="navbar fixed-top navbar-expand-sm bg-primary navbar-dark justify-content-center" style="min-height:50px">
     <ul class="nav-item">
-        <img src="logo.png" alt="Logo" style="width:60px;">
-    </ul>
+		<img src="logo.png" alt="Logo" style="width:60px;">
+	</ul>
     <ul class="nav-item">
-        <a class="navbar-brand justify-content-center display-1 mb-1 font-weight-normal" style="font-size:35px" href="#">ANJALAI AMMAL MAHALINGAM ENGINEERING COLLEGE</a>
-    </ul>
+		<a class="navbar-brand justify-content-center display-0 mb-0 font-weight-normal" style="font-size:35px" href="#">ANJALAI AMMAL MAHALINGAM ENGINEERING COLLEGE</a>
+	</ul>
 </nav>
-<body>
-<form action="" method="post">
-<input class="btn btn-lg btn-primary btn-block" type="submit" value="ENTER INTO STUDENT INFORMATION SYSTEM" name="b1"><br><br>
-
-</form>
-</body></html>
-<?php
-if(isset($_POST['b1']))
-{
-    header("location:login.php");
-}
-?>
+    <form class="form-signin justify-content-center text-center" style="padding-top:0px" method="post">
+    <br><br><br><br><br><br><br>
+      <i class="fa fa-address-book-o " style="font-size:36px"></i>
+      <h1 class="h2 mb-3 font-weight-normal" style="padding-bottom:0px">Student Information</h1>
+      <label for="uname"  class="sr-only">Username</label>
+      <input type="text" id="uname" class="form-control" style="height:50px;" placeholder="Username" name="name" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" style="height:50px;" name="psw" placeholder="Password" required><br>
+	  <input class="btn btn-lg btn-primary btn-block" type="submit" value="Sign In"><br><br>
+	  <p class="mt-0 mb-5 text-muted">&copy; AAMEC(IT Dept)-Abinesh,Aswin</p>
+    </form>
+  </body>
+</html>
